a99bb069079d98905ed010d77689918574a59c54
MorandellMichael <michael.morandell@tfobz.net>
2023-01-26 12:17:32 +0100

    added homepage

---
+++ OpenSkySouthTyrol/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  mySidebar:any = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  open_sidebar(): void {
+    this.mySidebar = document.getElementById("mySidebar");
+    if (this.mySidebar.style.display === 'block') {
+      this.mySidebar.style.display = 'none';
+    } else {
+      this.mySidebar.style.display = 'block';
+    }
+  }
+
+  close_sidebar(): void {
+    this.mySidebar.style.display = "none";
+  }
+
+}
